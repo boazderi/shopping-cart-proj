daecf1636980c77542138198a148673521dee589
Boaz Deri <boazderi@gmail.com>
2022-12-20 14:27:32 +0200

    updated

--- src/views/item-cart.vue
+++ src/views/item-cart.vue
@@ -1,6 +1,7 @@
 <template>
     <section class="item-details flex align-center justify-center">
-        <h3>details fot item {{ _id }}</h3>
+        <h3>My items</h3>
+        
         <button @click="goTo()">Go back</button>
     </section>
 </template>
