f76fd028e3c50042d4cd578dcd02285bc23fe389
Boaz Deri <boazderi@gmail.com>
2022-12-20 15:49:24 +0200

    updated

--- src/views/item-cart.vue
+++ src/views/item-cart.vue
@@ -3,7 +3,7 @@
     <h3 class="my-items-title">My items</h3>
     <div v-for="(item, idx) in items" :key="idx" class="flex item-row align-center">
       <div class="item-cart-title">{{ item.title }}</div>
-      <div>Price: {{'$' + item.price}} </div>
+      <div>Price: {{item.price + '€'}} </div>
       <div class="cart-img-container">
         <img :src="`src/assets/images/${item.title}.jpg`" width="30" alt="" />
       </div>
@@ -15,48 +15,6 @@
 <script>
 export default {
   name: "item-details",
-  data() {
-    return {
-    //   items2: [
-    //     {
-    //       id: "i101",
-    //       title: "item1",
-    //       price: "101",
-    //       imgPath: "src/assets/images/food1.jpg",
-    //     },
-    //     {
-    //       id: "i102",
-    //       title: "item2",
-    //       price: "102",
-    //       imgPath: "src/assets/images/food2.jpg",
-    //     },
-    //     {
-    //       id: "i103",
-    //       title: "item3",
-    //       price: "103",
-    //       imgPath: "src/assets/images/food3.jpg",
-    //     },
-    //     {
-    //       id: "i104",
-    //       title: "item4",
-    //       price: "104",
-    //       imgPath: "src/assets/images/food4.jpg",
-    //     },
-    //     {
-    //       id: "i105",
-    //       title: "item5",
-    //       price: "105",
-    //       imgPath: "src/assets/images/food5.jpg",
-    //     },
-    //     {
-    //       id: "i106",
-    //       title: "item6",
-    //       price: "106",
-    //       imgPath: "src/assets/images/food7.jpg",
-    //     },
-    //   ],
-    };
-  },
   created() {
   },
   methods: {
